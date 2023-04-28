9dbafc1712c84c2e78a13f3e080d87d233fd841a
André Moreira <18507576+alom2@users.noreply.github.com>
2023-04-28 00:29:25 -0300

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <main className="main-container">
+      <header className="header">search</header>
+      <section className="list-view">list</section>
+      <footer className="footer">
+        done by{" "}
+        <a href="https://www.linkedin.com/in/andr%C3%A9-moreira-a81266a7/">
+          @André Moreira
+        </a>
+      </footer>
+    </main>
+  );
+};
